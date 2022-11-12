a13bc928d2fe1fdad48e753402f5f5a38288c8ed
nguyendat2211 <117360628+nguyendat2211@users.noreply.github.com>
2022-11-12 11:32:35 +0700

    Commit2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "AnhnqCommit1N", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Commmit2", Toast.LENGTH_SHORT).show();
     }
 }
